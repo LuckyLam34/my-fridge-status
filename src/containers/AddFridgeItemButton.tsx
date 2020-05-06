df3f91202aa5b7dfac9b6e08dfe3e8479401f58b
Trinh Lam <TrinhTuLam34@gmail.com>
2020-05-06 17:45:41 +0700

    Added: Add fridge item button

--- src/containers/AddFridgeItemButton.tsx
+++ src/containers/AddFridgeItemButton.tsx
@@ -1,14 +1,67 @@
 import React from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
-export class AddFridgeItemButton extends React.Component {
+export class AddFridgeItemButton extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
+    this.state = {
+      show: false
+    };
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  componentWillMount() {
+
+  }
+
+  handleClose() {
+    this.setState({
+      show: !this.state.show
+    })
   }
 
   render() {
     return (
       <div className="add-fridge-item-button">
-        <button type="button" className="btn btn-primary">Primary</button>
+        <div className="add-fridge-item-button">
+          <button onClick={this.handleClose} type="button" className="btn btn-primary">
+            <i className="fas fa-plus"></i>&nbsp;
+            Add Item</button>
+        </div>
+        <Modal centered={true} show={this.state.show} onHide={this.handleClose} backdrop="static">
+          <Modal.Header closeButton>
+            <Modal.Title>Add New Item</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form>
+              <div className="form-group">
+                <label htmlFor="">Select vegetable</label>
+                <select className="custom-select">
+                  {/* <option selected>Open this select menu</option> */}
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div className="d-flex">
+                <div className="form-group">
+                  <label>Add new vegetable</label>
+                  <input type="text" className="form-control" aria-describedby="emailHelp" placeholder="" />
+                </div>
+                <button type="button" className="btn btn-primary mb-2"> <i className="fas fa-plus"></i>&nbsp;Add</button>
+              </div>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+          </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+          </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     )
   }
